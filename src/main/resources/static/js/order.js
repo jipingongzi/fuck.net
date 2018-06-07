a60251a643d9e2e924c4578923f55eb58212cb9c
dkxtx <278028476@qq.com>
2018-06-07 14:56:37 +0800

    订单信息

---
+++ src/main/resources/static/js/order.js
@@ -0,0 +1,15 @@
+var builder = {};
+//请求接口路径
+builder.url = "/Backstage/Order/GetDatagrid";
+//表格id
+builder.tableId = "#tab";
+//分页按钮id
+builder.pageBtnId = "#btn";
+builder.buildLine = function (item) {
+  var line ="<tr>";
+  //封装列的值，需要和table中的顺序一致
+  var content = buildTd(item.IDX,item.OrderNo,item.Phone,item.GoodsAmount,item.TotalPrice,item.Balance,item.Subsidy,item.InsertTime,item.PickupDate,item.SupplyName,item.IsDeliver,item.SupplyOfGoods,item.ReceiveTime);
+  line += content;
+  line += "</tr>";
+  return line;
+}
