5bf7e3c65f8c5e57a41aa48870d4ec51ec06c00c
xz <435032183@qq.com>
2018-06-07 14:32:59 +0800

    [add] 用户列表

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,8 +1,8 @@
-var builder = {};
-builder.url = "/Backstage/Member/GetDatagrid";
-builder.tableId = "#tabUser";
-builder.pageBtnId = "#btnUser";
-builder.buildLine = function (item) {
+var builderUser = {};
+builderUser.url = "/Backstage/Member/GetDatagrid";
+builderUser.tableId = "#tabUser";
+builderUser.pageBtnId = "#btnUser";
+builderUser.buildLine = function (item) {
     var line ="<tr>";
     var content = buildTd(item.Phone,item.Money,item.Balance,item.Subsidy,
         item.OrderPrice,item.OrderRechargeTotal,item.OrderSubsidyTotal,item.RefundBalance,item.RefundSubsidy,
@@ -11,32 +11,86 @@ builder.buildLine = function (item) {
     line += "</tr>";
     return line;
 }
+var builderCost = {};
+builderCost.url = "/Backstage/Member/GetWithholdDatagrid";
+builderCost.tableId = "#tabCost";
+builderCost.pageBtnId = "#btnCost";
+builderCost.buildLine = function (item) {
+    var line ="<tr>";
+    var content = buildTd(item.Phone,item.OldSubsidyTotal,item.OldBalanceTotal,item.SubsidyTotal,
+        item.BalanceTotal,item.NewSubsidyTotal,item.NewBalanceTotal,item.InsertTime,item.Summary,
+        item.HandleingByPerson);
+    line += content;
+    line += "</tr>";
+    return line;
+}
+var pageUser = {};
+pageUser.number = 0;
+pageUser.rows = 2;
+var pageCost = {};
+pageCost.number = 0;
+pageCost.rows = 2;
+//默认加载用户列表页
+getRow(1);
+
 $(function(){
     $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
         var activeTab = $(e.target).text();
         if(activeTab == "用户列表"){
-            builder.url = "/Backstage/Member/GetDatagrid";
-            builder.tableId = "#tabUser";
-            builder.pageBtnId = "#btnUser";
-            builder.buildLine = function (item) {
-                var line ="<tr>";
-                var content = buildTd(item.OrderPrice,item.OrderRechargeTotal,item.OrderSubsidyTotal,item.RefundBalance,item.RefundSubsidy,
-                    item.WithholdTotal,item.WithholdRechargePrice,item.WithholdSubsidyTotal,item.SupplyName,item.InsertTime,item.Status);
-                line += content;
-                line += "</tr>";
-                return line;
-            }
+           getRow(1);
         }else {
-            builder.url = "/Backstage/Supply/GetDatagrid";
-            builder.tableId = "#tab";
-            builder.pageBtnId = "#btn";
-            builder.buildLine = function (item) {
-                var line ="<tr>";
-                var content = buildTd(item.rn,item.Name,item.AccountCount,item.FarmCount,item.MemberCount,item.AgencyBuyingCount,item.AgencyBuyingProductCount,item.AgencyBuyingSellProductCount,item.AgencyBuyingSellProductTotal);
-                line += content;
-                line += "</tr>";
-                return line;
-            }
+            getRow(2);
         }
     });
 });
+
+
+function getRow(type){
+    var builder;
+    var page;
+    if(type == 1){
+        builder = builderUser;
+        page = pageUser;
+    }else {
+        builder = builderCost;
+        page = pageCost;
+    }
+    $.ajax({
+        type: "POST",
+        url: builder.url,
+        data: {page:page.number,rows:page.rows},
+        dataType: "json",
+        success: function(data){
+            var rows = data.rows;
+            if(rows == undefined || rows.length < page.rows){
+                $(builder.pageBtnId).text("没有更多了");
+                $(builder.pageBtnId).removeClass("btn-info");
+                $(builder.pageBtnId).addClass("btn-default");
+                if(rows == undefined){
+                    rows = [];
+                }
+            }
+            for(var j = 0; j < rows.length; j++) {
+                var line = builder.buildLine(rows[j])
+                $(builder.tableId).append(line);
+            }
+            page.number++;
+        },
+        error:function(){
+            $(builder.pageBtnId).text("没有更多了");
+            $(builder.pageBtnId).removeClass("btn-info");
+            $(builder.pageBtnId).addClass("btn-default");
+        }
+    });
+}
+function back() {
+    window.location.href = "../pages/nav.html";
+}
+function buildTd(){
+    var content = "";
+    for(var j = 0; j < arguments.length; j++) {
+        var field = arguments[j];
+        content += "<td>" + field + "</td>";
+    }
+    return content;
+}
