5b556117ffa995c97d035b301569b26c47664fd3
xz <435032183@qq.com>
2018-07-19 15:03:22 +0800

    [add] 删除老版本代码

--- src/main/java/com/example/service/query/IBuyProxyQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.service.query;
-
-import java.util.List;
-import java.util.Map;
-
-public interface IBuyProxyQueryService extends BaseQueryService {
-
-    List<Map<String,Object>> getBuyProxyList(int pageNumber,int pageSize);
-}
--- src/main/java/com/example/service/query/ISystemQueryService.java
+++
@@ -1,8 +0,0 @@
-package com.example.service.query;
-
-import java.util.List;
-
-public interface ISystemQueryService {
-
-    List<String> getUserAccounts(long serviceId);
-}
